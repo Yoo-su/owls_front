238bead1b28b2a4ec6f46f5690f5974b80a77d90
yoosu <rhan0871@naver.com>
2022-11-10 00:24:44 +0900

    각종 타입 작성

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,13 +6,30 @@ export interface UserSliceType {
 
 export interface PostSliceType {
     posts: PostType[];
+    postsLoading: boolean;
 }
 
 /* 컴포넌트 관련 타입들 */
+export interface CommentType {
+    commentId: string;
+
+}
+
 export interface PostType {
+    post_id: string;
+    post_text: string;
+    post_image?: string;
+    post_date: string;
+    post_user: string;
+    user_email: string;
+    user_image: string;
+    user_nickname: string;
+}
+
+export interface CreatePostType {
     postText: string;
-    postImage?: string;
+    file?: FormData;
     postedDate: string;
-    author: string;
-    authorProfileImg: string;
+    user: string;
+    fileName: string;
 }
