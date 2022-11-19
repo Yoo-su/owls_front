4cfa486d95e720a9b8750d8818037f1515f4679d
yoosu <rhan0871@naver.com>
2022-11-20 01:31:01 +0900

    함수 반환타입 지정"

--- src/utils/getLocalUser.ts
+++ src/utils/getLocalUser.ts
@@ -1,4 +1,12 @@
-const getLocalUser = () => {
+interface User {
+    user_email: string;
+    user_avatar: string;
+    user_name: string;
+    user_nickname: string;
+    user_id: number;
+}
+
+const getLocalUser = (): User | null => {
     const user = localStorage.getItem("user");
     if (user) {
         return JSON.parse(user);
