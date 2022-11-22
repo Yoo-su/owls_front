72ce749e84777bc2c017bd72126e80fcb22ca717
yoosu <rhan0871@naver.com>
2022-11-22 13:43:52 +0900

    스토어 옵션 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -14,6 +14,7 @@ const rootReducer = combineReducers({
 
 const store = configureStore({
     reducer: rootReducer,
+    devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
